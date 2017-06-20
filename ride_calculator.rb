2a257ef1a4583b6f2dbc1dc2fbc7fb937c66a9a7
Megan Trieu <mtrieu2359@gmail.com>
2017-06-20 22:58:57 +0000

    uber finished

--- ride_calculator.rb
+++ ride_calculator.rb
@@ -11,13 +11,17 @@
 # puts uberx_fare_calculator(2, 10)
 
 def farecalc(miles, minutes, type)
-    #{type}
+    type
 
-uberx=(0.97*miles)+(0.14*minutes)+0.40+1.58
-uberxl=(1.68*miles)+(0.26*minutes)+2.15+1.70
-uberselect=(2.17*miles)+(0.33*minutes)+1.70+4.02
-uberblack=(3.33*miles)+(0.44*minutes)+1.80+7.22
+if type == "uberx"
+    puts (0.97*miles)+(0.14*minutes)+0.40+1.58
+elsif type == "uberxl"
+    puts (1.68*miles)+(0.26*minutes)+2.15+1.70
+elsif type == "uberselect"
+    puts (2.17*miles)+(0.33*minutes)+1.70+4.02
+elsif type == "uberblack"
+    puts (3.33*miles)+(0.44*minutes)+1.80+7.22
 end
-
-puts farecalc(2, 10, "uberx")
+end
+puts farecalc(2, 10, "uberblack")
     
